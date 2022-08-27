332323d315c2c28b8ede98b7d5acc919c47f778f
Francisco Alvarez <41089629+falvarezb@users.noreply.github.com>
2022-08-27 22:14:42 +0100

    first test

--- src/main/scala/com/github/falvarezb/Assembler.scala
+++ src/main/scala/com/github/falvarezb/Assembler.scala
@@ -1,5 +1,43 @@
 package com.github.falvarezb
 
+import java.io.FileOutputStream
+import scala.collection.mutable
+import scala.io.Source
+
 object Assembler:
-  def assemble(asmFileName: String): Either[String,Unit] =
+
+  val symbolTable = mutable.HashMap[String, Int]()
+
+
+
+  def assemble(asmFileNamePath: String): Either[String,Unit] =
+    doLexicalAnalysis(asmFileNamePath)
     Right(())
+
+
+  def doLexicalAnalysis(asmFileNamePath: String) =
+    val source = Source.fromFile(asmFileNamePath)
+    val tokenizedLines = source.getLines().map(_.split(' ').filterNot(_.isEmpty)).filterNot(_.isEmpty).toList
+
+    val instructions = tokenizedLines.map { line =>
+      if line(0) == ".ORIG" then 0x0030
+      else if line(0) == "ADD" then 0x2110
+      else if line(0) == "HALT" then 0x25f0
+      else 0
+    }.filterNot(_ == 0)
+
+    val objFile = new FileOutputStream("src/test/resources/t1.obj")
+
+    instructions.foreach { instr =>
+      println(instr.toHexString)
+      //swap bytes (because of little-endian representation)
+      val (mostSignificantByte, leastSignificantByte) = (instr >> 8, instr & ((1 << 8) - 1))
+      objFile.write(leastSignificantByte + 256)
+      objFile.write(mostSignificantByte)
+    }
+    objFile.close()
+
+
+
+
+
--- src/test/scala/com/github/falvarezb/AssemblerTest.scala
+++ src/test/scala/com/github/falvarezb/AssemblerTest.scala
@@ -1,19 +1,19 @@
 package com.github.falvarezb
 
-import org.scalatest.funspec.AnyFunSpec
+import org.scalatest.funsuite.AnyFunSuite
 import org.scalatest.matchers.should.Matchers
 
 import java.io.FileInputStream
 import java.util
 import scala.io.Source
 
-class AssemblerTest extends AnyFunSpec with Matchers:
+class AssemblerTest extends AnyFunSuite with Matchers:
 
   def runTest(asmFileName: String): Unit =
     val path = "src/test/resources"
     Assembler.assemble(s"$path/$asmFileName") shouldBe Right(())
 
-    val asmFileNameWithoutExtension = asmFileName.split("\\.")(0)
+    val asmFileNameWithoutExtension = asmFileName.split('.')(0)
     val expectedFile = new FileInputStream(s"$path/$asmFileNameWithoutExtension.expected.obj")
     val actualFile = new FileInputStream(s"$path/$asmFileNameWithoutExtension.obj")
 
@@ -21,10 +21,9 @@ class AssemblerTest extends AnyFunSpec with Matchers:
     expectedFile.close()
     actualFile.close()
 
-  describe("t1") {
-    it("assembler output should match expected binary") {
-      runTest("t1.asm")
-    }
+  test("t1 output should match expected binary") {
+    runTest("t1.asm")
   }
+
 
 
