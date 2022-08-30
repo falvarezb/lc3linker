d7271ad56a80800689a5ff61e5dec3e2dd54126a
Francisco Alvarez <41089629+falvarezb@users.noreply.github.com>
2022-08-30 23:30:10 +0100

    refactoring

--- src/main/scala/com/github/falvarezb/Util.scala
+++ src/main/scala/com/github/falvarezb/Util.scala
@@ -1,34 +1,35 @@
 package com.github.falvarezb
+
 import cats.*
 import cats.implicits.*
 
 object Util {
 
   def parseNumericValue(token: String, lineNumber: LineNumber): Either[String, Int] =
+    Either.catchOnly[NumberFormatException] {
+      token(0) match
+        case '#' =>
+          //decimal literal
+          Integer.parseInt(token.substring(1))
+        case 'x' =>
+          //hex literal
+          Integer.parseInt(token.substring(1), 16)
+        case _ =>
+          //decimal literal without prefix
+          Integer.parseInt(token)
+    }.leftMap(_ => s"ERROR (line ${lineNumber.value}): Immediate $token is not a numeric value")
 
-      Either.catchOnly[NumberFormatException] {
-        token(0) match
-          case '#' =>
-            //decimal literal
-            Integer.parseInt(token.substring(1))
-          case 'x' =>
-            //hex literal
-            Integer.parseInt(token.substring(1), 16)
-          case _ =>
-            //decimal literal without prefix
-            Integer.parseInt(token)
-      }.leftMap( _ => s"ERROR (line ${lineNumber.value}): Immediate $token is not a numeric value")
-
-  def validateNumberRange(value: Int, lineNumber: LineNumber, lowerBound: Int, upperBound: Int): Either[String, Int] =
-    if value < lowerBound || value > upperBound then s"ERROR (line ${lineNumber.value}): Immediate operand ($value) outside of range ($lowerBound to $upperBound)".asLeft[Int]
-    else value.asRight[String]
+  def validateNumberRange(value: Int, lineNumber: LineNumber, lowerBound: Int, upperBound: Int): Either[String, Unit] =
+    if value < lowerBound || value > upperBound then
+      s"ERROR (line ${lineNumber.value}): Immediate operand ($value) outside of range ($lowerBound to $upperBound)".asLeft[Unit]
+    else ().asRight[String]
 
 
   def parseMemoryAddress(token: String, lineNumber: LineNumber): Either[String, Int] =
     for
-      x <- parseNumericValue(token, lineNumber)
-      y <- validateNumberRange(x, lineNumber, 0, 0xFFFF)
-    yield y
+      num <- parseNumericValue(token, lineNumber)
+      _ <- validateNumberRange(num, lineNumber, 0, 0xFFFF)
+    yield num
 
 
 }
