5d917ee2578babc5676677322b2068d05763e87f
Francisco Alvarez <41089629+falvarezb@users.noreply.github.com>
2022-09-10 20:59:58 +0100

    comments

--- src/main/scala/com/github/falvarezb/Util.scala
+++ src/main/scala/com/github/falvarezb/Util.scala
@@ -5,6 +5,11 @@ import cats.implicits.*
 
 object Util {
 
+  /**
+   * Integer literals in LC-3 assembly language can be represented as:
+   * - decimal values, either prefixed by '#' or without prefix
+   * - hex values prefixed by 'x'
+   */
   def parseNumericValue(token: String, lineNumber: LineNumber): Either[String, Int] =
     Either.catchOnly[NumberFormatException] {
       token(0) match
@@ -24,13 +29,26 @@ object Util {
       s"ERROR (line ${lineNumber.value}): Immediate operand ($value) outside of range ($lowerBound to $upperBound)".asLeft[Unit]
     else ().asRight[String]
 
-
+  /**
+   * Transforms the given token in a valid memory address, namely: an integer in the range [0, 0xFFFF]
+   */
   def parseMemoryAddress(token: String, lineNumber: LineNumber): Either[String, Int] =
     for
       num <- parseNumericValue(token, lineNumber)
       _ <- validateNumberRange(num, lineNumber, 0, 0xFFFF)
     yield num
 
+  /**
+   * Transforms the given token in a valid offset.
+   * 
+   * The token can be a symbolic name (label) or a numeric value. If a label, the corresponding memory address is
+   * retrieved from the symbol table.
+   * 
+   * How to work out the offset:
+   * - calculate difference between numeric value of the token and the instruction's memory address and then subtract 1
+   * - validate the result of the previous step is within the range indicated by offsetNumBits
+   * - calculate 2's complement
+   */
   def parseOffset(token: String, lineNumber: LineNumber, instructionMemoryAddress: InstructionMemoryAddress, offsetNumBits: Int, symbolTable: Map[String, InstructionMemoryAddress]): Either[String, Int] =
     def twosComplement(offset: Int) = if offset < 0 then offset + (1 << offsetNumBits) else offset
     
