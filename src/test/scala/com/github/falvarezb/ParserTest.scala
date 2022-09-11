1eaa8ff757024c6821fba3c58944c8d80574be11
Francisco Alvarez <41089629+falvarezb@users.noreply.github.com>
2022-09-11 20:44:40 +0100

    stringz tests

--- src/test/scala/com/github/falvarezb/ParserTest.scala
+++ src/test/scala/com/github/falvarezb/ParserTest.scala
@@ -7,9 +7,14 @@ import org.scalatest.matchers.should.Matchers
 class ParserTest extends AnyFunSpec with Matchers:
 
   describe(".STRINGZ parser") {
-    it("chars outside the quotations marks") {
+    it("chars before the quotations marks") {
       val lineMetadata = LineMetadata(".STRINGZ  a \"string content\"", List(".STRINGZ", "DOES NOT MATTER"), LineNumber(1))
       Parsers.parseStringz(lineMetadata) shouldBe Left("ERROR (line 1): Bad string ('.STRINGZ  a \"string content\"')")
+    }
+
+    it("chars after the quotations marks") {
+      val lineMetadata = LineMetadata(".STRINGZ \"string content\" a", List(".STRINGZ", "DOES NOT MATTER"), LineNumber(1))
+      Parsers.parseStringz(lineMetadata) shouldBe Left("ERROR (line 1): Bad string ('.STRINGZ \"string content\" a')")
     }
 
     it("one quotations mark missing") {
