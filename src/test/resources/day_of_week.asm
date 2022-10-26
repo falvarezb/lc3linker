3a4552c249d82269c2132374eca12054606ca12b
Francisco Alvarez <41089629+falvarezb@users.noreply.github.com>
2022-10-26 23:49:38 +0100

    refactoring asm examples

--- src/test/resources/day_of_week.asm
+++ src/test/resources/day_of_week.asm
@@ -14,111 +14,115 @@
 ;          
     .ORIG x3000
 
-    ;; Read day of month
-    AND R1, R1, #0
-    ADD R1, R1, #2
-    JSR READ_MULTI_DIGIT
-    ADD R6, R2, #0
+    ;;;;;;;;;;;;;;;;;;;;;;;
+    ;; day of month
+    ;;;;;;;;;;;;;;;;;;;;;;;
+    LEA R0,DAY_MONTH_PROMPT
+    PUTS 
+    AND R1,R1,#0
+    ADD R1,R1,#2
+    JSR READ_MULTI_DIGIT    ; R1 -> R2
+    ADD R6,R2,#0            ; f = k
 
+    ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+    ;; month
+    ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+    LEA R0,MONTH_PROMPT
+    PUTS 
+    JSR READ_MULTI_DIGIT    ; R1 -> R2
+    ST R2,MONTH
+    ; calculate m
+    ADD R0,R2,#-2        
+    BRp MONTH_GT_2_1
+    ADD R0,R0,#12           
+MONTH_GT_2_1              
+    ST R0,m
+    
+    ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+    ;; year
+    ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+    LEA R0,YEAR_PROMPT
+    PUTS 
+    AND R1,R1,#0
+    ADD R1,R1,#4
+    JSR READ_MULTI_DIGIT    ; R1 -> R2
 
-    ;LD R6,DAY_OF_THE_MONTH    ; R6 is f
-
-    ; calculate m and D
-
-    ;; Read year
-    AND R1, R1, #0
-    ADD R1, R1, #4
-    JSR READ_MULTI_DIGIT
-    ADD R1, R2, #0
-    ST R1,YEAR          
+    ; calculate year
+    LD R0,MONTH
+    ADD R0,R0,#-2        
+    BRp MONTH_GT_2_2
+    ADD R2,R2,#-1
+MONTH_GT_2_2
+    ST R2,YEAR
+    ; calculate D
+    ADD R1,R2,#0             
     LD R2,HUNDRED
-    JSR DIVISION        ; R1 is overwritten with value of D (R1,R2) -> (R0,R1)
-                        ; R2 can be discarded
+    JSR DIVISION        ; (R1,R2) -> (R0,R1) ; R1 = D                            
     ST R1,D
-    ;; Read month
-    AND R1, R1, #0
-    ADD R1, R1, #2
-    JSR READ_MULTI_DIGIT
-    ADD R0, R2, #0
-    ;LD R0,MONTH     ; calculate month m and store in R0
-    ADD R2,R0,#-2   ; R2 can be discarded
-    BRp MONTH_GT_2
-    ADD R0,R0,#12
-    LD R1,D
-    ADD R1,R1,#-1   ; using previous year   
-    ST R1,D 
-MONTH_GT_2
-    LD R1,D
-    ADD R0,R0,#-2   ; m = R0
-    ST R1,D         ; R1 can be discarded
 
-    ; calculate 13m
+    ; 13m
+    LD R0,m
     AND R1,R1,#0
     ADD R1,R1,#13
-    JSR MULTIPLICATION  ; R2 contains result of multiplication
-                        ; R0 and R1 can be discarded
+    JSR MULTIPLICATION  ; (R0,R1) => R2                        
     
-    ; calculate 13m-1
+    ; 13m-1
     ADD R2,R2,#-1       ; 13m-1 = R2
 
-    ; calculate (13m-1)/5
+    ; (13m-1)/5
     ADD R1,R2,#0        ; preparing R1 and R2 to call division
     AND R2,R2,#0
     ADD R2,R2,#5
-    JSR DIVISION        ; R0 contains quotient
-                        ; R1 and R2 can be discarded
-
-    ADD R6,R6,R0        ; k+(13m−1)/5 = R6
-                        ; R0 can be discarded
-    ; calculate D
+    JSR DIVISION  
+    ; k+(13m−1)/5
+    ADD R6,R6,R0        
+                        
+    ; k+(13m−1)/5 + D
     LD R1,D          
-    ADD R6,R6,R1        ; k+(13m−1)/5 + D = R6
+    ADD R6,R6,R1        
 
-    ; calculate D/4
+    ; D/4
     AND R2,R2,#0
     ADD R2,R2,#4
-    JSR DIVISION        ; R1 is discarded
-                        ; R0 contains quotient
-
-    ADD R6,R6,R0        ; k+(13m−1)/5 + D + D/4 = R6
-                        ; R0 can be discarded
+    JSR DIVISION        
+    ; k+(13m−1)/5 + D + D/4
+    ADD R6,R6,R0      
+                        
     ; calculate C
     LD R1,YEAR          
     LD R2,HUNDRED
-    JSR DIVISION        ; R0 is overwritten with value of C
-                        ; R1 and R2 can be discarded
+    JSR DIVISION                               
     ST R0,C
 
-    ; calculate C/4
+    ; C/4
     ADD R1,R0,#0
     AND R2,R2,#0
     ADD R2,R2,#4
-    JSR DIVISION        ; R0 is overwritten with value of quotient
-                        ; R1 and R2 can be discarded
-
-    ADD R6,R6,R0        ; k+(13m−1)/5 + D + D/4 + C/4 = R6
-                        ; R0 can be discarded
-    ; calculate -2C
+    JSR DIVISION                                
+    ; k+(13m−1)/5 + D + D/4 + C/4
+    ADD R6,R6,R0        
+                        
+    ; -2C
     LD R0,C
     ADD R0,R0,R0
     NOT R0,R0
     ADD R0,R0,#1
-
-    ADD R6,R6,R0        ; k+(13m−1)/5 + D + D/4 + C/4 - 2C = R6
-                        ; R0 can be discarded
+    ; k+(13m−1)/5 + D + D/4 + C/4 - 2C
+    ADD R6,R6,R0        
+    ST R6,f
+                        
     ; f%7
-    ADD R5,R6,#0
+    ADD R6,R6,#0    ; set condition codes
     BRzp POSITIVE_F
-    NOT R5,R5
-    ADD R5,R5,#1
+    NOT R6,R6
+    ADD R6,R6,#1
 POSITIVE_F
-    ADD R1,R5,#0
+    ADD R1,R6,#0
     AND R2,R2,#0
     ADD R2,R2,#7
-    JSR DIVISION        ; R1 contains remainder
-                        ; R0,R2 can be discarded
+    JSR DIVISION        ; R1 contains remainder                        
 
-    ADD R6,R6,#0
+    LD R6,f
     BRzp POSITIVE_DAY_OF_WEEK
     NOT R1,R1
     ADD R1,R1,#1
@@ -146,16 +150,17 @@ OUTPUT
     RET
 
 
-DAY_OF_THE_MONTH    .FILL #5
-MONTH               .FILL #1
-YEAR                .BLKW 1;.FILL #2022
+MONTH               .BLKW 1
+YEAR                .BLKW 1
 HUNDRED             .FILL #100
+f                   .BLKW 1
+m                   .BLKW 1
 C                   .BLKW 1
 D                   .BLKW 1
-R3_SAVE             .BLKW 1
-R4_SAVE             .BLKW 1
-R5_SAVE             .BLKW 1
 R7_SAVE             .BLKW 1
+DAY_MONTH_PROMPT    .STRINGZ         "Type day of the month\n"
+MONTH_PROMPT        .STRINGZ         "Type month\n"
+YEAR_PROMPT         .STRINGZ         "Type year\n"
 
 DAYS    
     .STRINGZ    "SUNDAY   \n"
